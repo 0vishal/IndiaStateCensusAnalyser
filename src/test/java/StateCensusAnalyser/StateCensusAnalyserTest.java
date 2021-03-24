35c7a482d9ef0b449d3b5c44b8c2c11f2ec44d63
0vishal <vishalsalaskar72@gmail.com>
2021-03-25 02:28:02 +0530

    check noofrecords

---
+++ src/test/java/StateCensusAnalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,27 @@
+package StateCensusAnalyser;
+
+import StateCensusAnalyser.StateCensusAnalyser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class StateCensusAnalyserTest {
+
+        private StateCensusAnalyser stateCensusAnalyser;
+        private String  path="C:/Users/ADMIN/Downloads/IndiaStateCensusData.csv";
+
+        @BeforeAll
+        public void execute() {
+            stateCensusAnalyser=new StateCensusAnalyser();
+        }
+
+        @Test
+        void NoofRecordMatches()  {
+            int size=stateCensusAnalyser.loadData(path);
+            System.out.println(size);
+            Assertions.assertEquals(29,size);
+
+        }
+
+    }
